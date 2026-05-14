56e2dfa6e5db3d02051b5041d8a8353d2cd03b78
Andriy <andrijbuluk@gmail.com>
2026-05-14 14:05:09 +0200

    Add links to pages and presentation of the page itself

--- security/urls.py
+++ security/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-
 from .views import (
     index,
+    GuardListView,
+    ClientListView,
 )
-
 
 urlpatterns = [
     path('', index, name='index'),
+    path("guard/", GuardListView.as_view(), name="guard-list"),
+    path("client/", ClientListView.as_view(), name="client-list"),
 ]
-
-
 
 app_name = "security"
--- security/views.py
+++ security/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Guard, Client
 
 
 def index(request):
-    pass
+    num_guard = Guard.objects.count()
+    num_client = Client.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_guard": num_guard,
+        "num_client": num_client,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "security/index.html", context=context)
+
+
+class GuardListView(generic.ListView):
+    model = Guard
+
+
+
+class ClientListView(generic.ListView):
+    model = Client
