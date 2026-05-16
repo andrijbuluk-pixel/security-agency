6281bbb694fc65a652baae42c32e417468e7fdb7
Andriy <andrijbuluk@gmail.com>
2026-05-16 11:14:47 +0200

    added defaul value in model Client and Guard

--- security/models.py
+++ security/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Client(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
 
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+        default="",
+    )
+
     first_name = models.CharField(
         max_length=255,
         blank=True,
@@ -36,11 +42,11 @@ class Equipment(models.Model):
 
 
 class Guard(models.Model):
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, default="")
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
-    license_number = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=255, default="")
     callsign = models.CharField(max_length=255)
     badge_number = models.CharField(max_length=255)
     contract_phone = models.CharField(max_length=255)
