0988e39d2ced858e0e72bf9f2e6aa4769a5620ce
Andriy <andrijbuluk@gmail.com>
2026-05-16 09:31:34 +0200

    added a new string uesername to the models.py

--- security/models.py
+++ security/models.py
@@ -1,10 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Client(models.Model):
-    pseudonym = models.CharField(max_length=255, unique=True)
-    email = models.EmailField()
-    password = models.CharField(max_length=64)
+class Client(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class ProtectionType(models.Model):
@@ -25,9 +23,11 @@ class Equipment(models.Model):
 
 
 class Guard(models.Model):
+    username = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
+    license_number = models.CharField(max_length=255)
     callsign = models.CharField(max_length=255)
     badge_number = models.CharField(max_length=255)
     contract_phone = models.CharField(max_length=255)
