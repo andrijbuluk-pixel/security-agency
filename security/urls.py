cfffac208d3791b11914dae66436653de44a19ee
Andriy <andrijbuluk@gmail.com>
2026-05-16 15:56:26 +0200

    template-guard: added a table with guards as well as search and creation of new guards

--- security/urls.py
+++ security/urls.py
@@ -4,12 +4,16 @@ from .views import (
     index,
     GuardListView,
     ClientListView,
+    GuardCreateView,
+
 )
 
 urlpatterns = [
     path('', index, name='index'),
     path("guard/", GuardListView.as_view(), name="guard-list"),
     path("client/", ClientListView.as_view(), name="client-list"),
+
+    path("guard/created/", GuardCreateView.as_view(), name="guard-created"),
 ]
 
 app_name = "security"
--- security/views.py
+++ security/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import GuardUsernameSearchForm, GuardCreationForm
 from .models import Guard, Client
 
 
@@ -22,6 +24,27 @@ def index(request):
 
 class GuardListView(generic.ListView):
     model = Guard
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(GuardListView, self).get_context_data(**kwargs)
+
+        context["search_form"] = GuardUsernameSearchForm()
+        return context
+
+    def get_queryset(self):
+        queryset = Guard.objects.prefetch_related("equipment")
+        username = self.request.GET.get("username", "")
+
+        if username:
+            return queryset.filter(username__icontains=username)
+        return queryset
+
+
+class GuardCreateView(generic.CreateView):
+    model = Guard
+    form_class = GuardCreationForm
+
 
 
 
