ffd148d8deed482059b8cb3e8b4a249870cc7e77
Andriy <andrijbuluk@gmail.com>
2026-05-20 10:52:22 +0200

    Added authorization URL with route

--- security_agency/urls.py
+++ security_agency/urls.py
@@ -18,8 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("security.urls", namespace="security")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
