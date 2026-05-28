0911d1e379894473002de0f13e9c03cbce4cd35f
Andriy <andrijbuluk@gmail.com>
2026-05-28 15:54:27 +0200

    fix: added manage.py in build.sh

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='user').exists() or User.objects.create_superuser('user', 'user@example.com', 'user12345')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='user').exists() or User.objects.create_superuser('user', 'user@example.com', 'user12345')"
