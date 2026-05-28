ae58531c465d95e30a7d127331e2f849181a599b
Andriy <andrijbuluk@gmail.com>
2026-05-28 14:09:56 +0200

    Added settings for .env.sample

---
+++ security_agency/settings/dev.py
@@ -0,0 +1,19 @@
+from security_agency.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
